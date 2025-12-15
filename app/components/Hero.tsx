0996e82f16b8f4d8da37495345a589ca0be65a46
Buttons Technology <buttonsafrotech@gmail.com>
2025-12-15 11:58:30 +0100

    fixed build error

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,25 +1,25 @@
-import { products } from '@/utils/constants'
-import Image from 'next/image'
-import React from 'react'
-import ProductCard from './ProductCard'
+// import { products } from '@/utils/constants'
+// import Image from 'next/image'
+// import React from 'react'
+// import {ProductCard} from './ProductCard'
 
-const Hero: React.FC = () => {
-    return (
-        <div className="w-full px-4 py-6 max-w-7xl mx-auto">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {products.map((product) => (
-                    <div key={product.id} className="flex">
-                        <ProductCard
-                            title={product.title}
-                            desc={product.desc}
-                            src={product.src}
-                            price={product.price}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
+// const Hero: React.FC = () => {
+//     return (
+//         <div className="w-full px-4 py-6 max-w-7xl mx-auto">
+//             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+//                 {products.map((product) => (
+//                     <div key={product.id} className="flex">
+//                         <ProductCard
+//                             title={product.title}
+//                             desc={product.desc}
+//                             src={product.src}
+//                             price={product.price}
+//                         />
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Hero
+// export default Hero
